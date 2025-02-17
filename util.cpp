3d9f995e275c5c756531d183ab2cb089efeef6ec
hkaylee <kayleeh@usc.edu>
2025-02-16 18:06:30 -0800

    util.cpp complete

--- util.cpp
+++ util.cpp
@@ -15,15 +15,29 @@ std::string convToLower(std::string src)
     to a set of words based on the criteria given in the assignment **/
 std::set<std::string> parseStringToWords(string rawWords)
 {
+// split string of many words into individual keywords 
+// define keyword of any string of 2 or more characters, split at each punctuation
+// store in common case - use convToLower
+// return a set- unique keys, unordered 
+    std::set<std::string> keywordSet;
+    std::string keyword;
 
+    for (std::string::iterator it = rawWords.begin(); it != rawWords.end(); ++it) {
+        if(std::isalpha(*it)){ // if char is valid alphabet
+            keyword += *it;   // append to keyword 
+        } else { // if reached punctuation mark don't append to keyword
+            if(keyword.size() >= 2){  // if up until puncutation is at least 2 char long
+                keywordSet.insert(convToLower(keyword));  // insert to set common case
+            }
+            keyword = ""; // reset keyword 
+        }
+    }
 
+    if(keyword.size() >= 2){ // if no puncutation, if word at least 2 characters long 
+        keywordSet.insert(convToLower(keyword)); // add to set  
+    }
 
-
-
-
-
-
-
+    return keywordSet; 
 
 }
 
